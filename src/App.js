115a8d53d8895c9d255f5209f2baad479dd6e3a7
alebreso <psbreso@gmail.com>
2018-10-05 14:22:26 +0200

    Cleaningcleaningcleaning

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
   render() {
     return(
       <div className="App">
-        <h1 className="question">Dog Breed Game</h1>
+        <h1 className="Title">Dog Breed Game</h1>
         <Question />
       </div>
     )
